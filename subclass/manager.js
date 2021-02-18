f3396cf653796f625617ea7e0648f409618e3ec5
mfranl <mflandry09@gmail.com>
2021-02-18 16:16:45 -0700

    added constructor functions for subclasses

---
+++ subclass/manager.js
@@ -0,0 +1,23 @@
+//subclass constructor function for employee
+
+class Manager {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+
+    getID() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+    
+}
+
+module.exports = Manager;
